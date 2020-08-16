abae5033d951725a4188104e440e84e72f964b64
Zygen <Xsaber64@Gmail.com>
2020-08-16 11:26:47 -0500

    Base game loop logic w/ attack and heal options for player, and monster attacking

--- game.py
+++ game.py
@@ -4,11 +4,12 @@ player_attack = 10
 player_heal = 12
 player_health = 100
 
-# List
-# player_info = [player_name, player_attack, player_heal, player_health]
-
-# Dictionary
+# Create player info (dictionary)
 player_info = {'name': player_name, 'attack': player_attack, 'heal': player_heal, 'health': player_health}
+
+# Print information of player
+print(player_info)
+
 # Create monster
 monster_info = {'name': 'Jake', 'attack': 15, 'health': 100}
 
@@ -25,50 +26,66 @@ while game_running == True:
 
     # Get input from user
     player_selection = input()
-
     # Check player choice
-
-    # Check if player is dead
-    if player_info['health'] <= 0:
-        game_running = False
-        print(player_info['name'] + " has died, game over!")
     
     # Attack the monster
     if player_selection == '1': 
-        # PLayer attacks 
-        print(player_info['name'] + " attacks " + monster_info['name'])
+        # Check if player dead
+        if(player_info['health'] <= 0):
+            monster_won = True
 
-        # Deal damage to the monster, equal to hp - player_attack
-        monster_info['health'] = monster_info['health'] -  player_info['attack']
+        else: 
+            # Player attacks 
+            print(player_info['name'] + " attacks " + monster_info['name'])
 
-        # Print how much monster has remaining
-        print(monster_info['name'] + " Has " + str(monster_info['health']) + " health remaining")
+            # Deal damage to the monster, equal to hp - player_attack
+            monster_info['health'] = monster_info['health'] -  player_info['attack']
 
-    # Heal
+            # Print how much monster has remaining
+            print(monster_info['name'] + " Has " + str(monster_info['health']) + " health remaining")
+
+    # Player Heals
     elif player_selection == '2':
+        # Check if player is dead
+        if(player_info['health'] <= 0):
+            monster_won = True
+
         # Player heals
-        print(player_info['name'] + " Heals themself for " + str(player_info['heal'])+ 'hp')
+        else:
+            print(player_info['name'] + " Heals themself for " + str(player_info['heal'])+ 'hp')
 
     # Else, invalid input, error
     else: 
         print('input invalid')
 
-    # Monster Attacks
-    print(monster_info['name'] + " attacks " + player_info['name'] + " and deals " + str(monster_info['attack']) + " damage")
-
-    # Subtract monster attack from player hp
-    player_info['health'] -= monster_info['attack']
-    print(player_info['name'] + ' has ' + str(player_info['health']) + ' health remaining')
-
-    # Check if player is dead
-    if player_info['health'] <= 0:
-        game_running = False
-        print(player_info['name'] + " has died, game over!")
-
-    # Check monster has died
+    # Check if monster is defeated
     if monster_info['health'] <= 0:
-        game_running = False
-        print(monster_info['name'] + " has been defeated!")
+        player_won = True
 
-# Print information of player
-print(player_info)
+    else:
+        # Monster Attacks
+        print(monster_info['name'] + " attacks " + player_info['name'] + " and deals " + str(monster_info['attack']) + " damage")
+
+        # Subtract monster attack from player hp
+        player_info['health'] -= monster_info['attack']
+        print(player_info['name'] + ' has ' + str(player_info['health']) + ' health remaining')
+    
+    # check if player or monster has died 
+    if player_info['health'] <= 0:
+        monster_won = True
+    if monster_info['health'] <= 0:
+        player_won = True
+
+    # Check if player or monster has won
+    if player_won == True or monster_won == True:
+        if player_won == True:
+            print(player_info['name'] + " Has defeated the monster!")
+        if monster_won == True:
+            print(player_info['name'] + " Has been defeated")
+        game_running = False
+
+    # If no, keep running game
+    else: 
+        pass
+
+
